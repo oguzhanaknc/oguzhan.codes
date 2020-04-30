901775957194145bb1b3ff1ca43cbecd918a1a99
oguzhanaknc <oguzhanaknc06@gmail.com>
2020-05-01 02:01:35 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueTailwind from "vue-tailwind";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHouseDamage,
+  faCode,
+  faTags,
+  faBookOpen,
+  faCoffee,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(faHouseDamage, faCode, faTags, faBookOpen, faCoffee);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
+Vue.use(VueTailwind);
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,155 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="flex min-h-screen bg-gray-800 overflow-auto">
+    <div
+      class="shadow-lg fixed h-screen w-20 block bg-gray-900 float-left z-10"
+    >
+      <ul>
+        <li
+          class="h-20 home text-white"
+          @mouseover="mouseover()"
+          @mouseleave="mouseleave()"
+        >
+          <div v-if="!isover">
+            <a href="#">
+              <img
+                class="h-20"
+                v-bind:src="require('../../public/' + img)"
+                alt="yok"
+              />
+            </a>
+          </div>
+          <div class="text-5xl" v-if="isover">
+            <a href="#">
+              <font-awesome-icon icon="house-damage" class="special" />
+            </a>
+          </div>
+        </li>
+
+        <li>
+          <a href="#">
+            <font-awesome-icon
+              icon="book-open"
+              class="text-gray-600 hover:text-red-500 text-2xl mt-12 transition duration-500"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <font-awesome-icon
+              icon="code"
+              class="text-gray-600  hover:text-red-500 text-2xl mt-12 transition duration-500"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <font-awesome-icon
+              icon="tags"
+              class="text-gray-600  hover:text-red-500 text-2xl mt-12 transition duration-500"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <font-awesome-icon
+              icon="coffee"
+              class="text-gray-600  hover:text-red-500 text-2xl mt-12 transition duration-500"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="absolute w-11/12 ml-24 z-0 mt-12 ">
+      <div class="text-white text-4xl text-left font-bold">
+        Quick Snippets and Code Samples
+      </div>
+      <hr class="border-dashed border-2 border-gray-600 " />
+    </div>
+    <div class="mt-40 m-auto mb-8 ">
+      <div
+        class=" transition duration-500 ease-in-out bg-gray-700 hover:bg-gray-600 transform hover:-translate-y-1 hover:scale-105 ...
+              mt-8 text-left  max-w-2xl rounded shadow-lg"
+      >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl text-white mb-2">
+            The Coldest Sunset
+          </div>
+          <p class="font-mono text-lg text-gray-400 text-base ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 py-4">
+          <span
+            class="inline-block bg-yellow-500 px-1 text-sm text-gray-300 mr-2"
+            >#photography</span
+          >
+          <span
+            class="inline-block bg-indigo-700 px-1 text-sm text-gray-300 mr-2"
+            >#travel</span
+          >
+        </div>
+      </div>
+      <div
+        class="transition duration-500 ease-in-out bg-gray-700 hover:bg-gray-600 transform hover:-translate-y-1 hover:scale-105 ...
+              mt-8 text-left  max-w-2xl rounded shadow-lg"
+      >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl text-white mb-2">
+            The Coldest Sunset
+          </div>
+          <p class="font-mono text-lg text-gray-400 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 py-4">
+          <span
+            class="inline-block bg-yellow-500 px-1 text-sm text-gray-300 mr-2"
+            >#photography</span
+          >
+          <span class="inline-block bg-blue-600 px-1 text-sm text-gray-300 mr-2"
+            >#travel</span
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import "../css/tailwind.css";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  data: function() {
+    return { img: "200.gif", isover: true };
+  },
+  methods: {
+    mouseover: function() {
+      this.isover = false;
+    },
+    mouseleave: function() {
+      this.isover = true;
+    },
+  },
 };
 </script>
+<style scoped>
+li {
+  display: block;
+}
+.home {
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    331deg,
+    rgba(2, 0, 36, 1) 0%,
+    #f96a1f 0%,
+    #fa8142 50%,
+    #fb9865 100%
+  );
+}
+</style>
